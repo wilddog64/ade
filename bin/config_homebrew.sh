2775c53907d43d675689dff69db485df112a7995
chengkai liang <chengkai.x.liang.-nd@disney.com>
2014-07-28 12:00:35 -0700

    homebrew install/configuration script

---
+++ bin/config_homebrew.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [[ $(uname) != 'Darwin' ]]; then
+    echo homebrew can only be installed for OSX Darwin, script $0 abort!
+fi
+
+brew_bin=$(which brew) 2>&1 > /dev/null
+if [[ $? != 0 ]]; then
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+else
+    echo $brew_bin exist!
+fi
+if [[ $? != 0 ]]; then
+    echo unable to install homebrew, script $0 abort!
+fi
