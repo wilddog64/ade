977f97b755f20386792f5931635df59f37f6228d
chengkai liang <chengkai.x.liang.-nd@disney.com>
2014-07-29 15:36:52 -0700

    add reload and vagrant-up options

--- bin/dev.sh
+++ bin/dev.sh
@@ -15,14 +15,28 @@ cd $SCRIPT_HOME/..
 SCRIPT_BIN=$SCRIPT_HOME/bin
 
 function init_devenv() {
+    local reload=$1
+
     install_homebrew
     install_brewcask
     install_vagrant_and_plugins
+
+    if [[ $reload = 1 ]]; then
+        vagrant reload --provision
+    else
+        vagrant up
+    fi
 }
 
 case "$1" in
     init)
         init_devenv
+        ;;
+    vagrant-up)
+        vagrant up
+        ;;
+    vagrant-reload)
+        vagrant reload --provision
         ;;
     update)
         git pull
