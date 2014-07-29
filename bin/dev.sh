e5707072ca44a03b57b10cd124df0a5af65f56d3
chengkai liang <chengkai.x.liang.-nd@disney.com>
2014-07-29 09:51:42 -0700

    create init_devenv function to install all the goodies for osx

--- bin/dev.sh
+++ bin/dev.sh
@@ -4,7 +4,7 @@ set -e
 
 # vagrant up
 
-source ./bin/config_homebrew.sh
+source ./bin/install_homebrew.sh
 source ./bin/install_brewcask.sh
 source ./bin/install_vagrant.sh
 
@@ -14,21 +14,15 @@ cd $SCRIPT_HOME/..
 
 SCRIPT_BIN=$SCRIPT_HOME/bin
 
+function init_devenv() {
+    install_homebrew
+    install_brewcask
+    install_vagrant_and_plugins
+}
+
 case "$1" in
 	init)
-		# install vagrant
-		# git clone all repos
-		# vagrant up (with /files mounted)
-		# provision a machine per shell scrips in /nodes
-		# check for homebrew then
-
-
-		# brew cask install chefdk;
-		# brew cask install vagrant;
-		# vagrant plugin install vagrant-omnibus;
-		# vagrant plugin install vagrant-berkshelf --plugin-version 2.0.1;
-		# vagrant reload --provison;
-		# vagrant up;
+        init_devenv
 		;;
 	update)
 		git pull
@@ -41,6 +35,3 @@ esac
 
 cd - > /dev/null
 
-function init_devenv() {
-    
-}
---
+++ bin/install_brewcask.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function install_brewcask() {
+    output=$(brew tap | grep cask)
+    if [[ $? = 0 ]]; then
+        echo caskroom/cask is already installed, script abort!
+    else
+        # tap 
+        brew tap caskroom/cask
+        brew install brew-cask
+    fi
+}
+
+install_brewcask
