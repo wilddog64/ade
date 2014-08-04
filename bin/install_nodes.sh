e3e30c12b40a3d2e238f8ed5b1ef96ecd1c84961
Adam Eivy <adam.eivy@disney.com>
2014-08-04 13:05:21 -0700

    migrate install_php to install_nodes script with TODO

---
+++ bin/install_nodes.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+# TODO: loop through the ../nodes directory
+# for each script in the directory, create a docker node and run the script content on boot
+# if the script is a chef_*.json file, then configure a docker node via chef cookbooks.
+# Example chef_disneyidui.json:
+# {
+#     "repo": "git://github.disney.com:DisneyID/DisneyID_UI_Chef.git",
+#     "env": "UI-Desktop"
+# }
