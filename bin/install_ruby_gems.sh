a248394ca71130744b89eae00c4a9d804f4e790c
chengkai liang <chengkai.x.liang.-nd@disney.com>
2014-08-04 14:47:32 -0700

    add functions to install correct ruby version, 1.9.3-p547

--- bin/install_ruby_gems.sh
+++ bin/install_ruby_gems.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+function install_rbenv() {
+    brew install rbenv
+    brew install ruby-build
+    if [[ $(grep rbenv ~/.bashrc) != 0 ]]; then
+        echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+    fi
+}
+
+function install_ruby_19() {
+    rbenv install 1.9.3-p547
+}
+
 function install_bundle() {
    gem install bundle
 }
@@ -13,6 +25,8 @@ function update_ruby_gems() {
 }
 
 function install_all_rubygems() {
+    install_rbenv
+    install_ruby_19
     install_bundle
     update_ruby_gems
     install_ruby_gems
