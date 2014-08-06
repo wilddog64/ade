988881b283a4d3920f2fe164469678d3a2ef395a
Adam Eivy <adam.eivy@disney.com>
2014-08-05 17:18:41 -0700

    better console log

--- bin/lib.sh
+++ bin/lib.sh
@@ -3,10 +3,10 @@
 function require_cask() {
     brew cask list $1
     if [[ $? != 0 ]]; then
-        echo $1 is not installed yet, installing...
+        echo '[missing] '$1'. installing...'
         brew cask install $1
         if [[ $? != 0 ]]; then
-            echo failed to install $1! aborting...
+            echo '[error] failed to install $1! aborting...'
             exit -1
         fi
     fi
@@ -14,10 +14,10 @@ function require_cask() {
 function require_brew() {
     brew list $1
     if [[ $? != 0 ]]; then
-        echo $1 is not installed yet, installing...
+        echo '[missing] '$1'. installing...'
         brew install $1
         if [[ $? != 0 ]]; then
-            echo failed to install $1! aborting...
+            echo '[error] failed to install $1! aborting...'
             exit -1
         fi
     fi
@@ -25,9 +25,9 @@ function require_brew() {
 function require_gem() {
     if [[ $(gem list --local | grep $1 | head -1 | cut -d' ' -f1) == $1 ]];
         then
-            echo $1' is already installed, skipping...';
+            echo '[ok] '$1' is already installed';
         else
-            echo $1' missing. installing...';
+            echo '[missing] '$1'. installing...';
             gem install $1
     fi
 }
