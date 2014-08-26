272faf6dc0b4a588f757c2b10ebbef24940d3fa6
Adam Eivy <Adam.Eivy@disney.com>
2014-08-26 16:23:34 -0700

    add port 80 forward

--- bin/run_image.sh
+++ bin/run_image.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -d -t docker-reg.cloud.corp.dig.com:5000/bde_devenv supervisord -c /etc/ade_supervisord.conf && tail -f /dev/null
+docker run -d -t -p 80:80 docker-reg.cloud.corp.dig.com:5000/bde_devenv supervisord -c /etc/ade_supervisord.conf && tail -f /dev/null
