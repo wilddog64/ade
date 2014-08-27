16bcc72bad699e1414723db032474a07d1dbbd37
chengkai liang <chengkai.x.liang.-nd@disney.com>
2014-08-27 08:22:03 -0700

    allow container to run and setup port forwarding

--- bin/run_image.sh
+++ bin/run_image.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-docker run -d -t -p 8001:8001 docker-reg.cloud.corp.dig.com:5000/bde_devenv supervisord -c /etc/ade_supervisord.conf && tail -f /dev/null
+ipaddr=$(ifconfig eth0 | perl -nle 'print $1 if s/inet addr:(.+?) /$1/g')
+echo docker run -d -t -p $ipaddr:8001:8001 docker-reg.cloud.corp.dig.com:5000/bde_devenv bash -c 'supervisord -c /etc/ade_supervisord.conf && tail -f /dev/null'
+docker run -d -t -p $ipaddr:8001:8001 docker-reg.cloud.corp.dig.com:5000/bde_devenv bash -c 'supervisord -c /etc/ade_supervisord.conf && tail -f /dev/null'
