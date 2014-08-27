a707faa2d29cebb64a828352df6ac4a2492aaef6
chengkai liang <chengkai.x.liang.-nd@disney.com>
2014-08-27 11:46:50 -0700

    allow mount local directories to container

--- bin/run_image.sh
+++ bin/run_image.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 ipaddr=$(ifconfig eth0 | perl -nle 'print $1 if s/inet addr:(.+?) /$1/g')
-echo docker run -d -t -p $ipaddr:8001:8001 docker-reg.cloud.corp.dig.com:5000/bde_devenv bash -c 'supervisord -c /etc/ade_supervisord.conf && tail -f /dev/null'
-docker run -d -t -p $ipaddr:8001:8001 docker-reg.cloud.corp.dig.com:5000/bde_devenv bash -c 'supervisord -c /etc/ade_supervisord.conf && tail -f /dev/null'
+mkdir -p /var/log/mysqld
+mkdir -p /var/log/httpd
+echo docker run -d -t -p $ipaddr:8001:8001 -v /var/log/mysqld:/var/log/mysqld -v /var/log/httpd:/var/log/httpd docker-reg.cloud.corp.dig.com:5000/bde_devenv bash -c 'supervisord -c /etc/ade_supervisord.conf && tail -f /dev/null'
+docker run -d -t -p $ipaddr:8001:8001 -v /var/log/mysqld:/var/log/mysqld -v /var/log/httpd:/var/log/httpd docker-reg.cloud.corp.dig.com:5000/bde_devenv bash -c 'supervisord -c /etc/ade_supervisord.conf && tail -f /dev/null'
