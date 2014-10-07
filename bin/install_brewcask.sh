4cc6eaae435c1c306abd6b5d05da5398ab06daa2
chengkai liang <ckm.liang@gmail.com>
2014-10-07 05:47:56 -0700

    fix problem not install brewk-cask

--- bin/install_brewcask.sh
+++ bin/install_brewcask.sh
@@ -4,8 +4,11 @@ function install_brewcask() {
     running "checking brew-cask"
     output=$(brew tap | grep cask)
     if [[ $? != 0 ]]; then
+	action "tap into brew-cask"
+	brew tap caskroom/homebrew-cask
+
 	action "installing brew-cask"
-	require_brew caskroom/cask/brew-cask
+	brew install brew-cask
     fi
 ok
 }
