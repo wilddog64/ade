a59b00c20d93f4940c150209108921397da02baf
chengkai liang <ckm.liang@gmail.com>
2014-10-07 04:56:21 -0700

    reformat function

--- bin/lib.sh
+++ bin/lib.sh
@@ -50,13 +50,14 @@ function require_cask() {
 }
 
 function require_brew() {
-    running "brew $1 $2"
-    brew list $1 > /dev/null 2>&1 | true
+    local brew=$1
+    running "brew $brew $2"
+    brew list $brew > /dev/null 2>&1 | true
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        action "brew install $1 $2"
-        brew install $1 $2
+        action "brew install $brew $2"
+        brew install $brew $2
         if [[ $? != 0 ]]; then
-            error "failed to install $1! aborting..."
+            error "failed to install $brew! aborting..."
             exit -1
         fi
     fi
