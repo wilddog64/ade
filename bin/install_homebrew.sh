7955b98507ef2bcf7cc091c3cd93aababea82c78
chengkai liang <ckm.liang@gmail.com>
2014-10-07 04:50:15 -0700

    update homebrew install location

--- bin/install_homebrew.sh
+++ bin/install_homebrew.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 function install_homebrew() {
-	running "checking homebrew"
+    running "checking homebrew"
     brew_bin=$(which brew) 2>&1 > /dev/null
     if [[ $? != 0 ]]; then
     	action "installing homebrew"
-        ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         if [[ $? != 0 ]]; then
         	error "unable to install homebrew, script $0 abort!"
         	exit -1
