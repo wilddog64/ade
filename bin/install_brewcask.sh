dc1d86dc1a491d8e382adb7de7c361be8dc88536
chengkai liang <ckm.liang@gmail.com>
2014-10-07 04:49:33 -0700

    reformat code

--- bin/install_brewcask.sh
+++ bin/install_brewcask.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 function install_brewcask() {
-	running "checking brew-cask"
+    running "checking brew-cask"
     output=$(brew tap | grep cask)
     if [[ $? != 0 ]]; then
-    	action "installing brew-cask"
-		require_brew caskroom/cask/brew-cask
+	action "installing brew-cask"
+	require_brew caskroom/cask/brew-cask
     fi
-    ok
+ok
 }
