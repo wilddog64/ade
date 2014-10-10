055aed4c8eac8b62ddd2ac8c28a96c4d2b0d7f24
chengkai liang <ckm.liang@gmail.com>
2014-10-10 13:22:18 -0700

    move require_brew function from lib.sh to install_homebrew.sh

--- bin/install_homebrew.sh
+++ bin/install_homebrew.sh
@@ -13,3 +13,19 @@ function install_homebrew() {
     fi
     ok
 }
+
+function require_brew() {
+    local brew=$1
+    running "brew $brew $2"
+    brew list $brew > /dev/null 2>&1 | true
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+        action "brew install $brew $2"
+        brew install $brew $2
+        if [[ $? != 0 ]]; then
+            error "failed to install $brew! aborting..."
+            exit -1
+        fi
+    fi
+    ok
+}
+
--- bin/lib.sh
+++ bin/lib.sh
@@ -14,21 +14,6 @@ function require_cask() {
     ok
 }
 
-function require_brew() {
-    local brew=$1
-    running "brew $brew $2"
-    brew list $brew > /dev/null 2>&1 | true
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-        action "brew install $brew $2"
-        brew install $brew $2
-        if [[ $? != 0 ]]; then
-            error "failed to install $brew! aborting..."
-            exit -1
-        fi
-    fi
-    ok
-}
-
 function require_gem() {
     running "gem $1"
     gem list --local | grep "$1\b"
