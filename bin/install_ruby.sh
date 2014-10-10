8db8f9c1372604f25e9f5541fd8cc4516c830e06
chengkai liang <ckm.liang@gmail.com>
2014-10-10 10:42:11 -0700

    move require_ruby from lib.sh to install_ruby.sh

--- bin/install_ruby.sh
+++ bin/install_ruby.sh
@@ -38,6 +38,18 @@ function install_ruby_bundle() {
     bundle install >> /dev/null
 }
 
+function require_ruby() {
+    local version=$1
+    running "need ruby version $version"
+    output=$(rbenv versions | grep $version);
+    if [[ $? != 0 ]]; then
+        action "installing ruby $version"
+        rbenv install $version
+        rbenv local $version
+    fi
+    ok
+}
+
 function install_ruby() {
     local ruby_version=$1
     install_rbenv
