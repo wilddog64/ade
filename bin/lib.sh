6681cd2b4a4aca577cc4c7564f05497b0906fa40
chengkai liang <ckm.liang@gmail.com>
2014-10-10 10:47:08 -0700

    move color definitions from lib.sh to console.sh

--- bin/lib.sh
+++ bin/lib.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# Colors
-ESC_SEQ="\x1b["
-COL_RESET=$ESC_SEQ"39;49;00m"
-COL_RED=$ESC_SEQ"31;01m"
-COL_GREEN=$ESC_SEQ"32;01m"
-COL_YELLOW=$ESC_SEQ"33;01m"
-COL_BLUE=$ESC_SEQ"34;01m"
-COL_MAGENTA=$ESC_SEQ"35;01m"
-COL_CYAN=$ESC_SEQ"36;01m"
-
 function require_cask() {
     running "brew cask $1"
     brew cask list $1 > /dev/null 2>&1 | true
