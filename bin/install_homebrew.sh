1dd456b832d6d80db185a705ccee4a38e7424578
chengkai liang <ckm.liang@gmail.com>
2014-10-12 07:05:28 -0700

    remove install_homebrew function (it is now in bootstrap.sh)

--- bin/install_homebrew.sh
+++ bin/install_homebrew.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 
-function install_homebrew() {
-    running "checking homebrew"
-    brew_bin=$(which brew) 2>&1 > /dev/null
-    if [[ $? != 0 ]]; then
-    	action "installing homebrew"
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        if [[ $? != 0 ]]; then
-            error "unable to install homebrew, script $0 abort!"
-            exit -1
-    	fi
-    fi
-    ok
-}
-
 function require_brew() {
     local brew=$1
+
+    PATH=/usr/local/bin:~/.linuxbrew/bin:$PATH
     running "brew $brew $2"
     brew list $brew > /dev/null 2>&1 | true
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
