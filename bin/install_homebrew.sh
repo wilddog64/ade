5b53eeb5d9e6ee5828cf1900f010fbe72a307228
chengkai liang <ckm.liang@gmail.com>
2014-10-13 07:13:25 -0700

    create install_rbenv to install and configure rbenv

--- bin/install_homebrew.sh
+++ bin/install_homebrew.sh
@@ -4,9 +4,11 @@ function require_brew() {
     local brew=$1
 
     PATH=/usr/local/bin:~/.linuxbrew/bin:$PATH
+    local brew_bin=$(which brew)
+    echo $brew_bin
     running "brew $brew $2"
-    brew list $brew > /dev/null 2>&1 | true
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    # brew list $brew > /dev/null 2>&1 | true
+    if [[ $(which rbenv) != 0 ]]; then
         action "brew install $brew $2"
         brew install $brew $2
         if [[ $? != 0 ]]; then
