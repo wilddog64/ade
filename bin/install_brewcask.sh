5abd9cbeeda31f0fc7377b7bb676c74d037bd39a
chengkai liang <ckm.liang@gmail.com>
2014-10-13 10:56:42 -0700

    allow smart brew-cask management between different linux platforms

--- bin/install_brewcask.sh
+++ bin/install_brewcask.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 function install_brewcask() {
+    action 'checking platform'
+    platform=$(get_os_platform)
+    if [[ "$platform" != "Darwin" ]]; then
+	warn "brew-cask only work for Darwin"
+	return 1
+    fi
+
     running "checking brew-cask"
     output=$(brew tap | grep cask)
     if [[ $? != 0 ]]; then
