6e19417a08725c350b501fdcc611e28ba2cdad7e
chengkai liang <ckm.liang@gmail.com>
2014-10-17 05:53:33 -0700

    fix require_brew logic problem that does not check if package exists before installing

--- bin/install_homebrew.sh
+++ bin/install_homebrew.sh
@@ -7,14 +7,17 @@ function require_brew() {
     local brew_bin=$(which brew)
     echo $brew_bin
     running "brew $brew $2"
-    # brew list $brew > /dev/null 2>&1 | true
     if [[ $(which rbenv) != 0 ]]; then
         action "brew install $brew $2"
-        brew install $brew $2
+        brew list $brew > /dev/null 2>&1
+        if [[ $? != 0 ]]; then
+            brew install $brew $2
+        fi
         if [[ $? != 0 ]]; then
             error "failed to install $brew! aborting..."
             exit -1
         fi
+        ok
     fi
     ok
 }
