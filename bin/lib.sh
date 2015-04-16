f0f97f204d0a3003ead1ec72ed622b1a24b8b794
chengkai liang <ckm.liang@gmail.com>
2015-04-15 18:27:02 -0700

    align the code

--- bin/lib.sh
+++ bin/lib.sh
@@ -51,12 +51,12 @@ function require_vagrant_plugin() {
     #echo 'checking if '$grepExpect' is installed via grepStatus: '$grepStatus
 
     if [[ $grepStatus != $grepExpect ]]; then
-            action "missing vagrant plugin $1 $2"
-            if [[ ! -z $vagrant_plugin_version ]]; then
-                vagrant plugin install $vagrant_plugin --plugin-version $vagrant_plugin_version
-            else
-                vagrant plugin install $vagrant_plugin
-            fi
+       action "missing vagrant plugin $1 $2"
+       if [[ ! -z $vagrant_plugin_version ]]; then
+           vagrant plugin install $vagrant_plugin --plugin-version $vagrant_plugin_version
+       else
+           vagrant plugin install $vagrant_plugin
+       fi
     fi
     ok
 }
